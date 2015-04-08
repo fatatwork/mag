15edd4e545a81615b88789d87adb5cc014111bd3
ukod <ukosname@mail.ru>
2015-04-08 12:46:25 +0300

    Добавлена анимация по событию (появление на экране)

--- scripts/searchElem.js
+++ scripts/searchElem.js
@@ -1,12 +1,34 @@
-var elements = window.document.getElementsByTagName('*');
+$(document).ready(function(){ /*Когда документ загружен*/
+    $('.rect').viewportChecker({
+        // Class to add to the elements when they are visible
+        classToAdd: 'visible', /*Добавляем класс видимости*/
+
+        // The offset of the elements (let them appear earlier or later)
+        offset: 100,
+
+        // Add the possibility to remove the class if the elements are not visible
+        repeat: false,
+
+         // Callback to do after a class was added to an element. Action will return "add" or "remove", depending if the class was added or removed
+        callbackFunction: addClassNow /*Функция добавляет id с CSS анимацией*/
+    });
+});
+
+function addClassNow(){
+    var elements = window.document.getElementsByTagName('*');/*Получаем все элементы*/
     if(elements.length == 0){
         alert("Crash");
     }
     var objects = [];
-    /*elements[0].className += " lol";*/
-    /*$("rate_rect1").addClass("lol");*/
     for(var i = 0; i < elements.length; ++i){
-        if(elements[i].id == "rate_rect1"){
-            elements[i].className += " lol";
+        if(/rect ./ig.test(elements[i].className)){
+            objects.push(elements[i]);/*Ищем класс*/
         }
     }
+
+    for(var i=0; i<objects.length; ++i){
+        if(objects[i].id == ""){
+            objects[i].id += "rate_rect" + (i+1);
+        }
+    }
+}
