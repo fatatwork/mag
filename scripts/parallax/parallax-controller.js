743bdc1687f60a78c7c01c1ecc3d6722d1a9818c
ukod <ukosname@mail.ru>
2015-04-20 14:47:55 +0300

    Некоторая оптимизация под разрешения

--- scripts/parallax/parallax-controller.js
+++ scripts/parallax/parallax-controller.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
 	//speedFactor - speed to move relative to vertical scroll.
 	//outerHeight - fix for image heigth. Default: 0.
 	//speedRounded - math round for speed amount. Default: false.
-	$('#title_actual').parallax("70%", 0.3, -40);
+	$('#title_actual').parallax("70%", 0.3);
 	$('#all-issues').parallax("50%", 0.5, -50, true);
 	$('#title_about').parallax("50%", 0.7, -300);
 	$('#title_separator').parallax("50%", 0.7, -300, true);
